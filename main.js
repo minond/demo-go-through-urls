6919afc139db76c106fb3998ecd6db992b9570c5
Marcos Minond <minond.marcos@gmail.com>
2015-05-31 14:25:42 -0600

    going through urls file

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var fs = require('fs'),
+    exec = require('child_process').exec;
+
+if (!process.argv[2]) {
+    console.log('Usage: %s urls_file', process.argv.join(' '));
+    process.exit(1);
+}
+
+var urls_file = process.argv[2],
+    reading = fs.createReadStream(urls_file);
+
+reading.on('data', function (data) {
+    var urls = data.toString().trim().split('\n');
+
+    urls.forEach(function (url) {
+        exec('phantomjs --ignore-ssl-errors=yes get-report-suite-id.js ' + url, function (err, stdout, stderr) {
+            console.log('%s: %s', url, stdout);
+        });
+
+        exec('phantomjs --ignore-ssl-errors=yes get-report-suite-id.js ' + url + ' 1', function (err, stdout, stderr) {
+            console.log('%s (mobile): %s', url, stdout);
+        });
+    });
+});
+
+// reading.on('end', function () {
+//     process.exit();
+// });
