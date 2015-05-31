29ecdc3cea92345f7f33c46cd9f6d7a8239d4f35
Marcos Minond <minond.marcos@gmail.com>
2015-05-31 14:14:23 -0600

    checking commandline args

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ var system = require('system'),
     webpage = require('webpage').create,
     page = webpage();
 
+if (!system.args[1]) {
+    console.log('Usage: %s url [mobile]', system.args[0]);
+    phantom.exit(1);
+}
+
 var url = system.args[1],
     mobile = system.args[2];
 
 if (mobile) {
+    // http://whatsmyuseragent.com/Devices/iPhone-User-Agent-Strings
     page.settings.userAgent = 'Mozilla/5.0(iPhone;U;CPUiPhoneOS4_0likeMacOSX;en-us)AppleWebKit/532.9(KHTML,likeGecko)Version/4.0.5Mobile/8A293Safari/6531.22.7';
 }
 
@@ -19,6 +25,7 @@ page.open(url, function (status) {
                 variable;
 
             for (variable in window) {
+                // TODO check
                 if (variable.indexOf('s_i_') === 0 && window[variable].src) {
                     report_suite_url = window[variable].src.match(report_suite_matcher);
 
