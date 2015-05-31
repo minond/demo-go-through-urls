95e969a90c9bb809a3f694bd93eaf6195ec47b5b
Marcos Minond <minond.marcos@gmail.com>
2015-05-31 14:04:25 -0600

    getting report suite

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 'use strict';
 
-var webpage = require('webpage'),
-    page = webpage.create();
+var webpage = require('webpage').create,
+    page = webpage();
 
 // page.settings.userAgent = '';
 
-page.open('https://www.google.com/', function (status) {
-    var title = page.evaluate(function () {
-        return document.title;
-    })
-    // console.log([].splice.call(arguments, 0))
-    console.log(title);
-    phantom.exit();
+page.open('http://www.walmart.com/', function (status) {
+    setTimeout(function () {
+        var report_suite_id = page.evaluate(function () {
+            var report_suite_matcher = /https?:\/\/.+?\/b\/ss\/(.+?)\//,
+                report_suite_url,
+                variable;
+
+            for (variable in window) {
+                if (variable.indexOf('s_i_') === 0 && window[variable].src) {
+                    report_suite_url = window[variable].src.match(report_suite_matcher);
+
+                    if (report_suite_url) {
+                        return report_suite_url;
+                    }
+                }
+            }
+        });
+
+        console.log(report_suite_id[1]);
+        phantom.exit();
+    }, 1000);
 });
