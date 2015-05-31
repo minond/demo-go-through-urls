2bfddc4efafcd1651a5e7118ba190d7137a8be70
Marcos Minond <minond.marcos@gmail.com>
2015-05-31 14:41:07 -0600

    writing report to fs

--- main.js
+++ main.js
@@ -11,26 +11,33 @@ if (!process.argv[2]) {
 var urls_file = process.argv[2],
     reading = fs.createReadStream(urls_file);
 
-reading.on('data', function (data) {
-    var urls = data.toString().trim().split('\n');
+var report_file = process.argv[3] || 'out.txt',
+    writing = fs.createWriteStream(report_file);
 
-    urls.forEach(function (url) {
-        exec('phantomjs --ignore-ssl-errors=yes get-report-suite-id.js ' + url, function (err, stdout, stderr) {
-            if (err) {
-                console.error('Error requesting %s', url);
-                return;
-            }
+writing.once('open', function () {
+    reading.on('data', function (data) {
+        var urls = data.toString().trim().split('\n');
 
-            console.log('%s (desktop): %s', url, stdout.trim());
-        });
+        urls.forEach(function (url) {
+            exec('phantomjs --ignore-ssl-errors=yes get-report-suite-id.js ' + url, function (err, stdout, stderr) {
+                if (err) {
+                    console.error('Error requesting %s', url);
+                    return;
+                }
 
-        exec('phantomjs --ignore-ssl-errors=yes get-report-suite-id.js ' + url + ' 1', function (err, stdout, stderr) {
-            if (err) {
-                console.error('Error requesting %s', url);
-                return;
-            }
+                console.log('%s (desktop): %s', url, stdout.trim());
+                writing.write(url + ' (desktop): ' + stdout.trim() + '\n');
+            });
 
-            console.log('%s (mobile): %s', url, stdout.trim());
+            exec('phantomjs --ignore-ssl-errors=yes get-report-suite-id.js ' + url + ' 1', function (err, stdout, stderr) {
+                if (err) {
+                    console.error('Error requesting %s', url);
+                    return;
+                }
+
+                console.log('%s (mobile): %s', url, stdout.trim());
+                writing.write(url + ' (mobile): ' + stdout.trim() + '\n');
+            });
         });
     });
 });
